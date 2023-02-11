307d02da277f2ff1af099239b63439bf23285293
jinzhongjia <jinzhongjia@qq.com>
2023-02-11 15:18:04 +0800

    init emacs config

---
+++ init.el
@@ -0,0 +1,7 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; This file bootstraps the configuration, which is divided into
+;; a number of other files.
+
+;;; Code:
