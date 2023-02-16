9449b2cda527e431eb356ea4b712cca9f7193bad
影翼 <yunyizhiying@gmail.com>
2023-02-16 16:13:57 +0800

    add ignore
    add use-package bootstrap

--- lisp/plugin.el
+++ lisp/plugin.el
@@ -1,7 +1,13 @@
 ;; for melpa package
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
