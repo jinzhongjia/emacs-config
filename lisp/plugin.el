149847ced14457063ee82eef01e30ada66b1a891
影翼 <yunyizhiying@gmail.com>
2023-02-16 20:13:15 +0800

    update initialize

--- lisp/plugin.el
+++ lisp/plugin.el
@@ -1,8 +1,8 @@
 ;; for melpa package
 (require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
