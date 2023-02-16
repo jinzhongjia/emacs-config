5cde27db0629fd66d2aff91752f314cb262607a5
影翼 <yunyizhiying@gmail.com>
2023-02-16 20:40:06 +0800

    add some default config for use-package

--- lisp/plugin.el
+++ lisp/plugin.el
@@ -16,6 +16,13 @@
   (package-install 'use-package))
 
 (eval-when-compile
+;; make use-package default behavior better
+;; with `use-package-always-ensure' you won't need ":ensure t" all the time
+;; with `use-package-always-defer' you won't need ":defer t" all the time
+  (setq use-package-always-ensure t
+        use-package-always-defer t
+        use-package-enable-imenu-support t
+        use-package-expand-minimally t)
   (require 'use-package))
 
 ;; for autoupdate package
