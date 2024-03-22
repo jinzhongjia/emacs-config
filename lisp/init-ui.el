372893ea5d3753a3296ef7b5b95ce75b524f95de
jinzhongjia <mail@nvimer.org>
2024-03-22 22:07:23 +0800

    update

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -21,4 +21,21 @@
   :config
   (load-theme 'vscode-dark-plus t))
 
+(use-package savehist
+  :ensure nil
+  :hook (after-init . savehist-mode)
+  :init (setq enable-recursive-minibuffers t ; Allow commands in minibuffers
+              history-length 1000
+              savehist-additional-variables '(mark-ring
+                                              global-mark-ring
+                                              search-ring
+                                              regexp-search-ring
+                                              extended-command-history)
+              savehist-autosave-interval 300)
+  )
+
+(use-package saveplace
+  :ensure nil
+  :hook (after-init . save-place-mode))
+
 (provide 'init-ui)
