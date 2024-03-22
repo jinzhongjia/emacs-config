a1ec4b1d1f4a6d062f46b9c363de71108e0c4935
jinzhongjia <mail@nvimer.org>
2024-03-22 23:48:06 +0800

    add complete support

--- lisp/init-enhance.el
+++ lisp/init-enhance.el
@@ -21,4 +21,50 @@
   (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
 )
 
+
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
+      (setq company-idle-delay 0.0)
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
+
+(use-package company-box
+      :ensure t
+      :if window-system
+      :hook (company-mode . company-box-mode))
+
+(use-package yasnippet
+  :ensure t
+  :hook
+  (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all)
+  ;; add company-yasnippet to company-backends
+  (defun company-mode/backend-with-yas (backend)
+    (if (and (listp backend) (member 'company-yasnippet backend))
+	    backend
+      (append (if (consp backend) backend (list backend))
+               '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  ;; unbind <TAB> completion
+  (define-key yas-minor-mode-map [(tab)]        nil)
+  (define-key yas-minor-mode-map (kbd "TAB")    nil)
+  (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+  :bind
+  (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
+    
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+
 (provide 'init-enhance)
