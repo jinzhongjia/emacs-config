b7c5aec713a25e09450f747e971ec0e86b10c5d7
jinzhongjia <mail@nvimer.org>
2024-03-22 19:49:04 +0800

    use elpaca

--- init.el
+++ init.el
@@ -36,3 +36,11 @@
     (load "./elpaca-autoloads")))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
+
+;; Block until current queue processed.
+(elpaca-wait)
