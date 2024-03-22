a22c5a15560aae0ae9875ea1a96ed25b84d15f84
jinzhongjia <mail@nvimer.org>
2024-03-23 00:01:24 +0800

    add some keybind for dirvish

--- lisp/init-enhance.el
+++ lisp/init-enhance.el
@@ -11,6 +11,12 @@
         '(nerd-icons file-time file-size collapse subtree-state vc-state git-msg))
   (setq dirvish-subtree-state-style 'nerd)
   (setq delete-by-moving-to-trash t)
+  (setq dired-mouse-drag-files t)                   ; added in Emacs 29
+  (setq mouse-drag-and-drop-region-cross-program t) ; added in Emacs 29
+  (setq mouse-1-click-follows-link nil)
+  (define-key dirvish-mode-map (kbd "<mouse-1>") 'dirvish-subtree-toggle-or-open)
+  (define-key dirvish-mode-map (kbd "<mouse-2>") 'dired-mouse-find-file-other-window)
+  (define-key dirvish-mode-map (kbd "<mouse-3>") 'dired-mouse-find-file)
   (setq dirvish-path-separators (list
                                  (format "  %s " (nerd-icons-codicon "nf-cod-home"))
                                  (format "  %s " (nerd-icons-codicon "nf-cod-root_folder"))
@@ -19,6 +25,26 @@
         "-l --almost-all --human-readable --group-directories-first --no-group")
   (dirvish-peek-mode) ; Preview files in minibuffer
   (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
+  (("C-c f" . dirvish-fd)
+   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
+   ("a"   . dirvish-quick-access)
+   ("f"   . dirvish-file-info-menu)
+   ("y"   . dirvish-yank-menu)
+   ("N"   . dirvish-narrow)
+   ("^"   . dirvish-history-last)
+   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
+   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-l" . dirvish-ls-switches-menu)
+   ("M-m" . dirvish-mark-menu)
+   ("M-t" . dirvish-layout-toggle)
+   ("M-s" . dirvish-setup-menu)
+   ("M-e" . dirvish-emerge-menu)
+   ("M-j" . dirvish-fd-jump))
 )
 
 
@@ -65,6 +91,5 @@
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
-
-
+  
 (provide 'init-enhance)
