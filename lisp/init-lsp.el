d53c1e0480342dfdb1fbb454116d05a2355eb0fb
jinzhongjia <mail@nvimer.org>
2024-07-22 20:15:00 +0800

    移除所有 el

--- lisp/init-lsp.el
+++
@@ -1,9 +0,0 @@
-;;; Code:
-(use-package lsp-bridge
-  :ensure '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
-            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-            :build (:not compile))
-  :init
-  (global-lsp-bridge-mode))
-
-(provide 'init-lsp)
--- lisp/init-theme.el
+++
@@ -1,9 +0,0 @@
-(use-package doom-themes
-  :config 
-  (load-theme 'doom-dark+ t))
-
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode))
-
-(provide 'init-theme)
